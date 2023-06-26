14f0fdce1f3d2d89fc8709d77567e5e155481ef7
sasaliacc04 <137411123+sasaliacc04@users.noreply.github.com>
2023-06-26 20:25:54 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -16,9 +16,9 @@ global.ONLY_GROUPS = false // Only Groups Use
 /*
 🤖 𝗕𝗢𝗧 𝗜𝗡𝗙𝗢 𝗦𝗘𝗧𝗧𝗜𝗡𝗚𝗦
 */
-global.OWNER_NAME = 'Dark Maker Ofc' // Owner Name
-global.OWNER_NUMBER = ['94715166712'] //Owner Number
-global.BOT_ADMINS = ['94715166712','94715166712'] //Bot admins numbers
+global.OWNER_NAME = 'ᴋɪɴɢ ꜱᴀꜱᴍɪᴛʜᴀ ᴀɪ ᴄʜᴀᴛ ʙᴏᴛ' // Owner Name
+global.OWNER_NUMBER = ['94719702508'] //Owner Number
+global.BOT_ADMINS = ['94719702508','94718002388'] //Bot admins numbers
 global.BLOCK_CHAT = [] // If you want Block Bot From Chats Use .jid Command and put Jids
 
 
@@ -27,7 +27,7 @@ global.BLOCK_CHAT = [] // If you want Block Bot From Chats Use .jid Command and 
 📌⭕ 𝗬𝗢𝗨 𝗠𝗨𝗦𝗧 𝗣𝗥𝗢𝗩𝗜𝗗𝗘 𝗕𝗘𝗙𝗢𝗥𝗘 𝗗𝗘𝗣𝗟𝗢𝗬 𝗕𝗢𝗧 📌⭕
 > Ai Bot Password Get From Watching This Video 
 */
-global.AI_PASSWORD = ''
+global.AI_PASSWORD = 'King-nima-gpt-bot'
 global.SESSION_ID = ''
 
 /*
