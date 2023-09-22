e0404f80f90e053c69048696ee25f66864c3666b
mickytexas <137762382+mickytexas@users.noreply.github.com>
2023-09-21 23:42:05 -0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -28,7 +28,7 @@ global.CAPTION = '*© 𝙶𝙴𝙽𝙴𝚁𝙰𝚃𝙴𝙳 𝙱𝚈 𝙾𝙿𝙴
 > Ai Bot Password Get From Watching This Video 
 */
 global.AI_PASSWORD = 'king-nima-gpt-bot'
-global.SESSION_ID = 'elisa_md@;;;YQsjFaiD#pN0FBKRbOWKMXmTuwHaz13Vvtw0WBH-gQS32CPahodE'
+global.SESSION_ID = ''
 
 /*
 🎲 𝗢𝗧𝗛𝗘𝗥 𝗦𝗘𝗧𝗜𝗡𝗚𝗦
